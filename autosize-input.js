c48b0e5151ecf2787238a611cd2e5566856e57ca
yuanqing <desk@yuanqing.sg>
2015-06-14 21:30:45 +0800

    Return the `set` function

--- autosize-input.js
+++ autosize-input.js
@@ -2,61 +2,67 @@
 
   'use strict';
 
-  // Reuse compiled regular expressions.
+  // Compile and cache the needed regular expressions.
   var SPACE = /\s/g;
   var LESS_THAN = /\>/g;
   var MORE_THAN = /</g;
 
   // We need to swap out these characters with their character-entity
   // equivalents because we're assigning the resulting string to
-  // `spacer.innerHTML`.
+  // `ghost.innerHTML`.
   function escape(str) {
     return str.replace(SPACE, '&nbsp;')
               .replace(LESS_THAN, '&lt;')
               .replace(MORE_THAN, '&gt;');
   }
 
-  // Inline styles to hide our `spacer` element.
-  var spacerStyle = 'box-sizing:content-box;display:inline-block;height:0;overflow:hidden;position:absolute;top:0;visibility:hidden;white-space:nowrap';
+  // Inline styles to hide the `ghost` element.
+  var ghostStyle = 'box-sizing:content-box;display:inline-block;height:0;overflow:hidden;position:absolute;top:0;visibility:hidden;white-space:nowrap';
 
   function autosizeInput(elem, opts) {
 
-    // Create the `spacer`.
-    var spacer = document.createElement('div');
-    document.body.appendChild(spacer);
+    // Force `content-box` on the `elem`.
+    elem.style.boxSizing = 'content-box';
 
-    // Apply the `font-size` and `font-family` styles of `elem` on the `spacer`.
+    // Create the `ghost` element.
+    var ghost = document.createElement('div');
+    document.body.appendChild(ghost);
+
+    // Apply the `font-size` and `font-family` styles of `elem` on the
+    // `ghost` element.
     var elemStyle = window.getComputedStyle(elem);
-    var cssText = spacerStyle +
+    ghost.style.cssText = ghostStyle +
       ';font-family:' + elemStyle.fontFamily +
       ';font-size:'   + elemStyle.fontSize;
-    spacer.style.cssText = cssText;
 
-    // Helper function that sets the contents of `spacer` to `str`, before
-    // copying the new width of `spacer` to `elem`.
+    // Helper function that sets the contents of `ghost` to `str`, before
+    // copying the new width of `ghost` to `elem`.
     function set(str) {
-      spacer.innerHTML = escape(str);
-      var width = window.getComputedStyle(spacer).width;
+      str = str || elem.value || elem.getAttribute('placeholder') || '';
+      ghost.innerHTML = escape(str);
+      var width = window.getComputedStyle(ghost).width;
       elem.style.width = width;
       return width;
     }
 
-    // Set the initial value of `spacer` to the `value` or `placeholder` of
-    // `elem`.
-    var width = set(elem.value || elem.getAttribute('placeholder') || '');
-
     // Call `set` on every `input` event (IE9+).
-    elem.addEventListener('input', function(e) {
-      set(e.target.value);
+    elem.addEventListener('input', function() {
+      set();
     });
 
-    // Set `min-width` if `opts.minWidth` was set.
-    if (opts && opts.minWidth) {
-      spacer.style.minWidth = width;
+    // Set the initial value of `ghost` to the `value` or `placeholder`
+    // of `elem`.
+    var width = set();
+
+    // Set `min-width` if `opts.minWidth` was set, and only if the initial
+    // width is non-zero.
+    if (opts && opts.minWidth && width !== '0px') {
+      ghost.style.minWidth = width;
     }
+
+    return set;
   }
 
-  /* istanbul ignore else */
   if (typeof module == 'object') {
     module.exports = autosizeInput;
   } else {
